1412442c844bd63bf435286527ec4fc09c176fa1
charanjeetkau <ckbss97@gmail.com>
2020-03-11 17:27:39 +0530

    employee present or absent? + Daily Employee Wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -2,7 +2,12 @@
 R=$(($RANDOM%2))
 if [[ $R -eq 0 ]]
 then
-			echo Present
+			echo Employee Present
 else
-			echo Absent
+			echo Employee Absent
 fi
+
+wage_per_hour=20
+full_day_hour=8
+daily_employee_wage=$(($wage_per_hour*$full_day_hour))
+echo Daily Employee Wage': ' $daily_employee_wage
