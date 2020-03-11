cde0585992c872a1fc479e5b66a93d69a9fe86cf
charanjeetkau <ckbss97@gmail.com>
2020-03-11 18:03:36 +0530

    added: employee part time wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -11,3 +11,8 @@ wage_per_hour=20
 full_day_hour=8
 daily_employee_wage=$(($wage_per_hour*$full_day_hour))
 echo Daily Employee Wage': ' $daily_employee_wage
+
+part_time=$(($full_day_hour/2))
+part_time_wage=$(($wage_per_hour*$part_time))
+echo 'Part Time: '$part_time ' Hours'
+echo 'Part Time Wage: '$part_time_wage
